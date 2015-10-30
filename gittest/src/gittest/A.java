47f382dfac28f710f4b1ce3e86d758b35f23880f
dingzeqi <dingzeqi@caitu99.com>
2015-10-30 14:53:23 +0800

    haha

--- gittest/src/gittest/A.java
+++ gittest/src/gittest/A.java
@@ -5,9 +5,6 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("123123123");
 		
-		
-		System.out.println("123123123");
-		
-		System.out.println("123123123");
+
 	}
 }
